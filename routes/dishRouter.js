31bdfca4b29a557391dc1fb08818bb02c2decbdc
Bethlehem Seid <betty53.msi@gmail.com>
2021-07-11 18:42:02 -0400

    passport JWT

--- routes/dishRouter.js
+++ routes/dishRouter.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const authenticate = require('../authenticate');
 
 const Dishes = require('../models/dishes');
 
@@ -18,7 +19,7 @@ dishRouter.route('/')
     .catch((err) => next(err));
 })
 
-.post((req,res,next) => {
+.post(authenticate.verifyUser, (req,res,next) => {
     Dishes.create(req.body)
     .then((dish) => {
         console.log('Dish Created', dish);
@@ -29,13 +30,13 @@ dishRouter.route('/')
     .catch((err) => next(err));
 })
 
-.put((req,res,next) => {
+.put(authenticate.verifyUser, (req,res,next) => {
     res.statusCode = 403;
     res.end('PUT operation not supported on /dishes');
 })
 
 
-.delete((req,res,next) => {
+.delete(authenticate.verifyUser, (req,res,next) => {
     Dishes.remove({})
     .then((resp) => {
         res.statusCode = 200;
@@ -58,12 +59,12 @@ dishRouter.route('/:dishId')
     .catch((err) => next(err));
 })
 
-.post((req,res,next) => {
+.post(authenticate.verifyUser, (req,res,next) => {
     res.statusCode = 403;
     res.end('POST operation not supported on /dishes/'+ req.params.dishId);
 })
 
-.put((req,res,next) => {
+.put(authenticate.verifyUser, (req,res,next) => {
     Dishes.findByIdAndUpdate(req.params.dishId, {
         $set: req.body
     }, {new: true})
@@ -75,7 +76,7 @@ dishRouter.route('/:dishId')
     .catch((err) => next(err));
 })
 
-.delete((req,res,next) => {
+.delete(authenticate.verifyUser, (req,res,next) => {
     Dishes.findByIdAndRemove(req.params.dishId)
     .then((resp) => {
         res.statusCode = 200;
@@ -105,7 +106,7 @@ dishRouter.route('/:dishId/comments')
     .catch((err) => next(err));
 })
 
-.post((req,res,next) => {
+.post(authenticate.verifyUser, (req,res,next) => {
     Dishes.findById(req.params.dishId)
     .then((dish) => {
         if(dish != null) {
@@ -126,13 +127,13 @@ dishRouter.route('/:dishId/comments')
     .catch((err) => next(err));
 })
 
-.put((req,res,next) => {
+.put(authenticate.verifyUser, (req,res,next) => {
     res.statusCode = 403;
     res.end('PUT operation not supported on /dishes/' + req.params.dishId + '/comments');
 })
 
 
-.delete((req,res,next) => {
+.delete(authenticate.verifyUser, (req,res,next) => {
     Dishes.findById(req.params.dishId)
     .then((dish) => {
         if(dish != null) {
@@ -181,13 +182,13 @@ dishRouter.route('/:dishId/comments/:commentId')
     .catch((err) => next(err));
 })
 
-.post((req,res,next) => {
+.post(authenticate.verifyUser, (req,res,next) => {
     res.statusCode = 403;
     res.end('POST operation not supported on /dishes/'+ req.params.dishId
         + '/comments/' + req.params.commentId);
 })
 
-.put((req, res, next) => {
+.put(authenticate.verifyUser, (req, res, next) => {
     Dishes.findById(req.params.dishId)
     .then((dish) => {
         if (dish != null && dish.comments.id(req.params.commentId) != null) {
@@ -220,7 +221,7 @@ dishRouter.route('/:dishId/comments/:commentId')
     .catch((err) => next(err));
 })
 
-.delete((req,res,next) => {
+.delete(authenticate.verifyUser, (req,res,next) => {
     Dishes.findById(req.params.dishId)
     .then((dish) => {
         if(dish != null && dish.comments.id(req.params.commentId) != null) {
--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,7 @@ var User = require('../models/user');
 
 var passport = require('passport');
 
-// var passport = require('passport');
+ var authenticate = require('../authenticate');
 
 var router = express.Router();
 router.use(bodyParser.json());
@@ -13,25 +13,6 @@ router.use(bodyParser.json());
 router.get('/', (req, res, next) => {
   res.send('respond with a resource');
 });
-
-// router.post('/signup', (req, res, next) => {
-//   User.register(new User({username: req.body.username}), 
-//     req.body.password, (err, user) => {
-//     if(err){
-//       res.statusCode = 500;
-//       res.setHeader('Content-Type', 'application/json');
-//       res.json({err: err});
-//     }
-//     else {
-//       passport.authenticate('local')(req, res, () => {
-//         res.statusCode = 200;
-//         res.setHeader('Content-Type', 'application/json');
-//         res.json({success: true, status: 'Registration Successful!'});
-//       });
-//     }
-//   });
-// });
-
 
 router.post('/signup', (req, res, next) => {
   User.register(new User({username: req.body.username}), 
@@ -52,16 +33,11 @@ router.post('/signup', (req, res, next) => {
   });
 });
 
-// router.post('/login', passport.authenticate('local'), (req, res) => {  
-//   res.statusCode = 200;
-//   res.setHeader('Content-Type', 'application/json');
-//   res.json({success: true, status: 'You are successfully logged in!'});
-// });
-
 router.post('/login', passport.authenticate('local'), (req, res) => {
+  var token = authenticate.getToken({_id: req.user._id});
   res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json');
-  res.json({success: true, status: 'You are successfully logged in!'});
+  res.json({success: true, token: token, status: 'You are successfully logged in!'});
 });
 
 router.get('/logout', (req, res) => {
